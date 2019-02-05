f0efddb884dd6a1d7413d6f6387db2993649c3d5
DiegoFernMart <diegofernmart@gmail.com>
2019-02-05 16:30:19 +0100

    5.2 - Adicionales

--- WebContent/foro.jsp
+++ WebContent/foro.jsp
@@ -8,20 +8,36 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Foro SDI- UO258367</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<c:if test="${sessionScope.usuario ==null}">
+
+<!-- Redirect login-->
+<c:if test="${sessionScope.usuario == null}">
 	<c:redirect url="/login.jsp" />
 </c:if>
 
 <!-- Contenido -->
 <div class="container" id="contenedor-principal">
 		<h2>5.2 Foro SDI - UO258367</h2>
-		<form class="form-Comentario" method="post" action="foro.jsp">
+		<form class="form-Comentario" method="post" action="addComentario">
 			<div>
 				<input type=text />
 				<button type="submit" class="btn btn-primary">Agregar</button>
+			</div>
+			<div>
+			<c:forEach var="com" items="${listaComentarios}">
+				<div>
+				<p>Author: ${com.getAuthor()}</p>
+				<p>${com.getMessage()}</p>
+				</div>
+			</c:forEach>
 			</div>
 		</form>
 	</div>
--- src/com/uniovi/sdi/ServletComentario.java
+++ src/com/uniovi/sdi/ServletComentario.java
@@ -1,6 +1,9 @@
 package com.uniovi.sdi;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletComentario
  */
-@WebServlet("/ServletComentario")
+@WebServlet("/addComentario")
 public class ServletComentario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +29,27 @@ public class ServletComentario extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Pares nombre imagen
+				List<Comentario> listaComentarios = (List<Comentario>) request.getSession().getAttribute("listaComentarios");
+				
+				
+				// No hay lista, creamos una y la insertamos en sesión
+				if (listaComentarios == null) {
+					listaComentarios = new LinkedList<Comentario>();
+					request.getSession().setAttribute("listaComentarios", listaComentarios);
+				}
+				//Insertamos los comentarios en la lista
+				ComentarioService cS= new ComentarioService();
+				List<Comentario> lista = cS.getComentarios();
+				for(Comentario c : lista){
+					if (c.getAuthor() != null)
+						listaComentarios.add(c);
+					
+				}
+				
+				// Retornar la vista con parámetro
+				request.setAttribute("listaComentarios", listaComentarios);
+				getServletContext().getRequestDispatcher("/foro.jsp").forward(request, response);
 	}
 
 	/**
